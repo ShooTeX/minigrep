7d582c95154d73d2a667f383e9fc3f4e4570a56a
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-12-21 23:11:59 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for `{}`", query);
-    println!("In file `{}`", file_path);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read file");
+    println!("Searching for `{}`", config.query);
+    println!("In file `{}`", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
 
     println!("With text:\n{contents}")
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
