2e2ae5fde0db50cba978fd0afc8484548ff3b655
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2022-12-21 23:33:55 +0100

    refactor into run fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,9 +11,18 @@ fn main() {
     println!("Searching for `{}`", config.query);
     println!("In file `{}`", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    println!("With text:\n{contents}")
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
